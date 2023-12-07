29361e052858bd6e1ec03550ef9edafa88dcebe4
dcdanny945 <147899532+dcdanny945@users.noreply.github.com>
2023-12-07 23:45:37 +1100

    Add files via upload

---
+++ Love Calculator.js
@@ -0,0 +1,8 @@
+
+prompt("What's your name?");
+let loverName=prompt("What's your lover's name?");
+let chance=Math.random();
+let chancePer= Math.round(chance*100)+1;
+
+
+alert("Your chane falling love with "+loverName+" is "+chancePer+"%.");
